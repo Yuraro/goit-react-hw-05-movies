2ea31e8857511b75773b19635ca913ada54da59a
Yuraro <romanykyriy@gmail.com>
2023-06-19 11:31:43 -0700

    Update MovieCard.jsx

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 
-const { notification } = require("components/Norification/Notification");
+
 const { Wraper, InfoWraper, Title, Score, UserScore, Owerview, GenresTitle, GenresList, Genres } = require("./MovieCard.styled");
 
 const MovieCard = ({ movieInfo }) => {
@@ -12,7 +12,7 @@ const MovieCard = ({ movieInfo }) => {
                 src={
                     movieInfo.poster_path
                         ? `https://image.tmdb.org/t/p/w300${movieInfo.poster_path}`
-                        : notification ('There is no photo avalible!')
+                        : []
                 }
                 alt="img"
             />
