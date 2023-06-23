96f8ee0736446198984830ced90befdfed1c50fb
Yuraro <romanykyriy@gmail.com>
2023-06-22 17:53:14 -0700

    Update

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -5,48 +5,50 @@ import MovieCard from 'components/MovieCard/MovieCard';
 import { AddInfo, AddInfoList, BackLink } from './MovieDetails.styled';
 
 const MovieDetails = () => {
-
     const location = useLocation();
     const backLinkHref = useRef(location.state?.from ?? '/');
 
-    const [movieInfo, setMovieInfo] = useState({});
     const { movieId } = useParams();
-
+    const [movieInfo, setMovieInfo] = useState(null);
 
     useEffect(() => {
-    if (movieInfo === {}) {
-        return;
-    }
-    const getInfo = async () => {
-        try {
-        const response = await getInfoMovie(movieId);
+        const getInfo = async () => {
+            try {
+                const response = await getInfoMovie(movieId);
+                setMovieInfo(response);
+            } catch (error) {
+                console.log(error);
+            }
+        };
 
-        setMovieInfo(response);
-        } catch (error) {
-        console.log(error);
+        if (movieId) {
+            getInfo();
         }
-    };
-    getInfo();
-    }, [movieInfo, movieId]);
+    }, [movieId]);
 
     return (
         <>
             <BackLink to={backLinkHref.current}>Back</BackLink>
-            <MovieCard movieInfo={movieInfo}></MovieCard>
-            <AddInfoList>
-                <AddInfo>
-                    <Link to="cast">Cast</Link>
-                </AddInfo>
-                <AddInfo>
-                    <Link to="reviews">Reviews</Link>
-                </AddInfo>
-            </AddInfoList>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Outlet />
-            </Suspense>
+            {movieInfo ? (
+                <>
+                    <MovieCard movieInfo={movieInfo} />
+                    <AddInfoList>
+                        <AddInfo>
+                            <Link to="cast">Cast</Link>
+                        </AddInfo>
+                        <AddInfo>
+                            <Link to="reviews">Reviews</Link>
+                        </AddInfo>
+                    </AddInfoList>
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <Outlet />
+                    </Suspense>
+                </>
+            ) : (
+                <div>Loading...</div>
+            )}
         </>
     );
-
 };
 
-export default MovieDetails
+export default MovieDetails;
